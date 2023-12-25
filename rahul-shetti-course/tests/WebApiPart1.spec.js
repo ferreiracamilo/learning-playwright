c7bc0cf8202aeaad24c4c6a35496a5e6fb77f484
ferreiracamilo_parqa <camilo.ferreira@partech.com>
2023-12-25 20:20:01 -0300

    add api test

--- rahul-shetti-course/tests/WebApiPart1.spec.js
+++ rahul-shetti-course/tests/WebApiPart1.spec.js
@@ -1,10 +1,20 @@
 const { test, expect, request } = require('@playwright/test');
+const loginPayload = {"userEmail":"anshika@gmail.com","userPassword":"Iamking@000"};
 
 test.beforeAll('Login with page object', async ({ page }) => {
     const apiContext = await request.newContext();
-    apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login")
+    const loginResponse = apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
+      {
+        data: loginPayload
+      });
+    expect((await loginResponse).ok()).toBeTruthy();
+    console.log("LoginResponse es > " + loginResponse);
+    const loginResponseJson = (await loginResponse).json();
+    console.log("LoginResponse es > " + loginResponseJson);
+    const sessionToken = loginResponseJson.token;
+    console.log("LoginResponse es > " + sessionToken);
 });
 
 test('Login with page object', async ({ page }) => {
-    
-  });
+  
+});
