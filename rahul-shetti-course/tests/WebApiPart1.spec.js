d907bd2975bcf0a253f74e72defe51ba3871cb6e
ferreiracamilo_parqa <camilo.ferreira@partech.com>
2023-12-26 02:01:55 -0300

    update

--- rahul-shetti-course/tests/WebApiPart1.spec.js
+++ rahul-shetti-course/tests/WebApiPart1.spec.js
@@ -1,20 +1,26 @@
 const { test, expect, request } = require('@playwright/test');
 const loginPayload = {"userEmail":"anshika@gmail.com","userPassword":"Iamking@000"};
+let sessionToken;
 
-test.beforeAll('Login with page object', async ({ page }) => {
-    const apiContext = await request.newContext();
-    const loginResponse = apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
-      {
-        data: loginPayload
-      });
-    expect((await loginResponse).ok()).toBeTruthy();
-    console.log("LoginResponse es > " + loginResponse);
-    const loginResponseJson = (await loginResponse).json();
-    console.log("LoginResponse es > " + loginResponseJson);
-    const sessionToken = loginResponseJson.token;
-    console.log("LoginResponse es > " + sessionToken);
+
+
+test.beforeAll('Login with page object', async () => {
+  const apiContext = await request.newContext();
+  const loginResponse = await apiContext.post(
+    "https://rahulshettyacademy.com/api/ecom/auth/login",
+    {
+      data: loginPayload
+    }
+  );
+  expect(loginResponse.ok()).toBeTruthy();
+  const loginResponseJson = await loginResponse.json();
+  sessionToken = loginResponseJson.token;
 });
 
 test('Login with page object', async ({ page }) => {
-  
+  page.addInitScript(value => {
+    window.localStorage.setItem("token", value);
+  }, sessionToken);
+  await page.goto("https://rahulshettyacademy.com/client/");
+  await page.getByText("ORDERS").isVisible();
 });
